27a2c14569f893f75ab5b07bb1cca616e5075db1
LeeJaeWoo <yeboykr@naver.com>
2019-09-10 21:54:42 +0900

    1. codlity lesson 7

--- app/src/main/java/com/namget/algorism/JavaActvity.java
+++ app/src/main/java/com/namget/algorism/JavaActvity.java
@@ -5,15 +5,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.namget.algorism.stacksQueues.Brackets;
 import com.namget.algorism.stacksQueues.Fish;
+import com.namget.algorism.stacksQueues.StoneWall;
 
 public class JavaActvity extends AppCompatActivity {
 
-    int a[] = {4, 3, 2, 1, 5};
-    int b[] = {0, 1, 0, 0, 0};
+    int a[] = {1};
+    int b[] = {8,8,5,7,9,8,7,4,8};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fish.solution(a, b);
+        StoneWall.solution(b);
     }
 }
--- app/src/main/java/com/namget/algorism/stacksQueues/StoneWall.java
+++ app/src/main/java/com/namget/algorism/stacksQueues/StoneWall.java
@@ -3,11 +3,12 @@ package com.namget.algorism.stacksQueues;
 import java.util.Stack;
 
 public class StoneWall {
-    public int solution(int[] H) {
+    public static int solution(int[] H) {
         // write your code in Java SE 8
         int result = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(H[0]);
+        result ++;
         for (int i = 0; i < H.length; i++) {
             if (H[i] > stack.peek()) {
                 stack.push(H[i]);
@@ -16,7 +17,11 @@ public class StoneWall {
                 while (!stack.empty() && H[i] < stack.peek()) {
                     stack.pop();
                 }
-                result++;
+                if(!stack.empty() && stack.peek() == H[i]){
+
+                }else{
+                    result++;
+                }
                 stack.push(H[i]);
             }
         }
