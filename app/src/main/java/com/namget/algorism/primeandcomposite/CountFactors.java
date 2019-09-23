af6b58d1b3c54e9725a4f480dfd59da43a0dea1b
LeeJaeWoo <yeboykr@naver.com>
2019-09-24 00:08:08 +0900

    1. codlity lesson 10

--- app/src/main/java/com/namget/algorism/primeandcomposite/CountFactors.java
+++ app/src/main/java/com/namget/algorism/primeandcomposite/CountFactors.java
@@ -1,6 +1,34 @@
 package com.namget.algorism.primeandcomposite;
 
-public class CountFactors {
+import java.util.*;
 
-    
+public class CountFactors {
+    public int solution(int N) {
+        // write your code in Java SE 8
+        int result = 1;
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int count = 0;
+        int beforeDiv = 2;
+        int div = 2;
+        while (N > 1) {
+            if (N % div == 0) {
+                N /= div;
+                hashMap.put(div, hashMap.getOrDefault(div, 0) + 1);
+                //다시 초기화 할필요가 없다
+                count ++;
+//                div = 2;
+            } else {
+                if(count > 0){
+                    result *= (count +1);
+                    count = 0;
+                }
+                div++;
+            }
+        }
+        if(count > 0){
+            result *= (count +1);
+        }
+
+        return result;
+    }
 }
