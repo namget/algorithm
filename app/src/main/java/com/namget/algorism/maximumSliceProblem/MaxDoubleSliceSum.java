bf3eff29800d31e329c2e6308d712256bab6db74
namget <yeboykr@naver.com>
2019-09-24 13:47:17 +0900

    lesson. 10

---
+++ app/src/main/java/com/namget/algorism/maximumSliceProblem/MaxDoubleSliceSum.java
@@ -0,0 +1,11 @@
+package com.namget.algorism.maximumSliceProblem;
+
+public class MaxDoubleSliceSum {
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+
+
+        return 0;
+    }
+}
--- app/src/main/java/com/namget/algorism/primeandcomposite/CountFactors.java
+++ app/src/main/java/com/namget/algorism/primeandcomposite/CountFactors.java
@@ -1,34 +1,16 @@
 package com.namget.algorism.primeandcomposite;
 
-import java.util.*;
-
 public class CountFactors {
     public int solution(int N) {
         // write your code in Java SE 8
-        int result = 1;
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        int count = 0;
-        int beforeDiv = 2;
-        int div = 2;
-        while (N > 1) {
-            if (N % div == 0) {
-                N /= div;
-                hashMap.put(div, hashMap.getOrDefault(div, 0) + 1);
-                //다시 초기화 할필요가 없다
-                count ++;
-//                div = 2;
-            } else {
-                if(count > 0){
-                    result *= (count +1);
-                    count = 0;
-                }
-                div++;
+        int result = 0;
+        //1..2..3..4..6..8..12..24
+        //즉 제곱근까지의 반까지 나눠지는 갯수 x 2 = 총 개수 
+        for(int i = 1; i <= Math.sqrt(N); i++){
+            if(N % i == 0){
+                result++;
             }
         }
-        if(count > 0){
-            result *= (count +1);
-        }
-
-        return result;
+        return result * 2;
     }
 }
